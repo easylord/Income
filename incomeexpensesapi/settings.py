50d8682333858631b7e67b0704c6b943091e49c4
Easylord <yusufisrael007@gmail.com>
2020-10-25 17:18:53 +0100

    few changes

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =env("SECRET_KEY")
+SECRET_KEY = '@6p-h7#oy4unyb4+(@i&3eq(knbkvjkeyv&@*8+a%f45b@mfm1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
